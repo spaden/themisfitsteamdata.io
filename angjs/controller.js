8fcab5bd0a2cecf0316457e6a71ae0313d7172a0
Dheshoju Kalyan Kumar <kalyanspade.n@gmail.com>
2018-12-09 18:30:52 +0530

    40% work done
    Plausible ui look needed

--- angjs/controller.js
+++ angjs/controller.js
@@ -1,6 +1,6 @@
 app.controller('Whole', function($scope, $location) {
     $scope.testing = "Cool man working";
     $scope.hey = function(event) {
-        console.log(event.target.id);
+        $location.path('/team/' + event.target.id);
     }
 });
--- angjs/tmcontroller.js
+++ angjs/tmcontroller.js
@@ -1,3 +1,57 @@
 app.controller('TmCont', function($scope, $routeParams) {
     $scope.hey = $routeParams.firstname;
-})
+
+    if ($routeParams.firstname == 'spaden') {
+        this.who = "Creator | Full Stack Developer | Project Manager";
+    } else if ($routeParams.firstname == 'asbn') {
+        this.who = "Tester | Team Manager | Hiring Manager";
+    } else {
+        this.who = "Team Manager";
+    }
+
+    $scope.test = this.who;
+
+    this.check = function(name) {
+        var obj;
+        if (name == 'dlg') {
+            obj = {
+                name: 'Dachupally Guru Lekhan Sai',
+                teamsize: 18,
+                rating: 4.5
+            }
+        } else if (name == 'rahul') {
+            obj = {
+                name: 'BethiReddy Rahul Reddy',
+                teamsize: 16,
+                rating: 3.9
+            }
+        } else if (name == 'nimesh') {
+            obj = {
+                name: 'Chanaburlu Nimesh Reddy',
+                teamsize: 10,
+                rating: 3
+            }
+        } else if (name == 'sumanth') {
+            obj = {
+                name: 'Sumanth Raja Naidu',
+                teamsize: 12,
+                rating: 3.2
+            }
+        } else if (name == 'asbn') {
+            obj = {
+                name: 'ASBN Sudhir',
+                teamsize: 20,
+                rating: 4.8
+            }
+        } else {
+            obj = {
+                name: 'Dheshoju Kalyan Kumar',
+                teamsize: 5,
+                rating: 2
+            }
+        }
+        return obj;
+    };
+
+    $scope.test2 = this.check($routeParams.firstname);
+});
